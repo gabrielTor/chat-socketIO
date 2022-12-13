eebc0b0533e417b54ceace5fba6110b9e4142391
gabrielTor <gabriel_torres2012@yahoo.com>
2022-12-13 19:11:56 -0300

    websocket server

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const app = express()
+const http = require("http")
+const { Server } = require("socket.io")
+const cors = require("cors")
+require('dotenv').config()
+
+app.use(cors());
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"],
+    },
+})
+
+io.on("connection", (socket) => {
+  socket.on("join_room", (data) => {
+    socket.join(data)
+  })
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data)
+  })
+})
+
+
+server.listen(process.env.PORT, () => {
+    console.log("WEBSOCKET SERVER IS RUNNING")
+});
