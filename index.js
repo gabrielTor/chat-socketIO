4bda6e131e027719168482705001cd3d5a11044a
gabrielTor <gabriel_torres2012@yahoo.com>
2022-12-16 00:26:32 -0300

    origin ecom-app-phi.vercel

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-      origin: "http://localhost:3000",
+      origin: "https://ecom-app-phi.vercel.app",
+      // origin: "http://localhost:3000",
       methods: ["GET", "POST"],
     },
 })
@@ -20,6 +21,11 @@ io.on("connection", (socket) => {
   socket.on("join_room", (data) => {
     socket.join(data)
   })
+  // socket.on('typing', (data)=>{
+  //   if(data.typing){
+  //     io.emit('display', data)
+  //   }
+  // })
   socket.on("send_message", (data) => {
     socket.to(data.room).emit("receive_message", data)
   })
