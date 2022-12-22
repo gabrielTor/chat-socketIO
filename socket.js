23b701d6871731c44527a690548325cff49d02a0
gabrielTor <gabriel_torres2012@yahoo.com>
2022-12-21 22:02:38 -0300

    ....

--- socket.js
+++ socket.js
@@ -8,7 +8,7 @@ async function sockets(io){
         })
 
         socket.on('typing', (data) => {
-            io.to(data.room).emit('display', data)
+            socket.to(data.room).emit('display', data)
         })
 
         socket.on("send_message", async(data) => {
